59a25f557d38c523520caeaa8255937b2ba5213f
akshaysyaduvanshi <akshaysyaduvanshi@gmail.com>
2018-07-04 15:07:26 +0530

    Added adj list implementation of the graph

--- src/Graph/ListGraph.kt
+++ src/Graph/ListGraph.kt
@@ -1,73 +1,30 @@
 package Graph
 
-import Queue.Queue
+class ListGraph<Node, Cost>(private val isDirected: Boolean = false) {
+    private val adjList: MutableMap<Node, MutableList<Pair<Node, Cost>>> = mutableMapOf()
 
-class ListGraph(nodes: Int) {
-    private val adjacencyList: Array<Vertex> = Array(nodes) {i -> Vertex(i)}
-
-    fun addEdge(from: Int, to: Int, value: Int, twoWayEdge: Boolean = true) {
-        addEdgeInternal(from, to, value)
-        if (twoWayEdge) {
-            addEdgeInternal(to, from, value)
+    fun addEdge(from: Node, to: Node, cost: Cost) {
+        createOrAddEdge(from, to, cost)
+        if (!isDirected) {
+            createOrAddEdge(to, from, cost)
         }
     }
 
-    fun printGraph() {
-        for(i in 0 until adjacencyList.size) {
-            print("Adjacency matrix of the vertex $i")
-            print(" head")
-            adjacencyList[i].edges.forEach {
-                print(" -> ${it.vertex.id}")
-            }
+    fun print() {
+        adjList.forEach { node, adjList ->
             println()
-        }
-    }
-
-    fun bfs(): MutableList<Int> {
-        val visitedArray = Array(adjacencyList.size) {false}
-        var queue = Queue(mutableListOf<Vertex>())
-        queue.enqueue(adjacencyList[0])
-        val dfsTraversal = mutableListOf<Int>()
-        while (!queue.isEmpty()) {
-            val node = queue.dequeue()!!
-            if (visitedArray[node.id]) { continue}
-            dfsTraversal.add(node.id)
-            visitedArray[node.id] = true
-            node.edges.forEach { queue.enqueue(it.vertex)}
-        }
-        dfsTraversal.forEach {
-            print(it)
-        }
-        return  dfsTraversal
-    }
-
-    fun dfs(): MutableList<Int> {
-        val dfsTraversal = mutableListOf<Int>()
-        val visitedArray = Array(adjacencyList.size) {false}
-
-        dfsUtil(adjacencyList[0], visitedArray)
-
-        dfsTraversal.forEach {
-            print(it)
-        }
-        return  dfsTraversal
-
-    }
-
-    private fun dfsUtil(vertex: Vertex, visited: Array<Boolean>) {
-        print(vertex.id)
-        visited[vertex.id] = true
-        vertex.edges.forEach {
-            if (!visited[it.vertex.id]) {
-                dfsUtil(it.vertex, visited)
+            println("Node ${node.toString()} makes an edge with")
+            adjList.forEach {
+                println("\tNode ${it.first.toString()} with an edge weight of = ${it.second.toString()}")
             }
         }
     }
 
-
-    private fun addEdgeInternal(from: Int, to: Int,  value: Int) {
-        val fromNode = adjacencyList[from]
-        val toNode = adjacencyList[to]
-        fromNode.edges.add(Edge(toNode, value))
+    private fun createOrAddEdge(from: Node, to: Node, cost: Cost) {
+        adjList[from]?.let {
+            it.add(Pair(to, cost))
+            return
+        }
+        adjList[from] = mutableListOf(Pair(to, cost))
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,13 @@
 import Graph.ListGraph
 
 fun main(args: Array<String>) {
-    val graph = ListGraph(5)
-    graph.addEdge(0,1, 1)
-    graph.addEdge(0,4, 1)
-    graph.addEdge(1,2, 1)
-    graph.addEdge(1,3, 1)
-    graph.addEdge(1,4, 1)
-    graph.addEdge(2,3, 1)
-    graph.addEdge(3,4, 1)
-    graph.dfs()
+    val graph = ListGraph<Int, Int>()
+    graph.addEdge(0, 1, 10)
+    graph.addEdge(0, 4, 20)
+    graph.addEdge(1, 2, 30)
+    graph.addEdge(1, 3, 40)
+    graph.addEdge(1, 4, 50)
+    graph.addEdge(2, 3, 60)
+    graph.addEdge(3, 4, 70)
+    graph.print()
 }
